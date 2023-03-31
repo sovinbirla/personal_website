50ab6257c3259067f0f2cdcf72ddcef0f428a8a3
sbirla2 <sbirla2@illinois.edu>
2023-03-31 16:57:44 -0500

    testing changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          test
+        </div>
       </header>
     </div>
   );
