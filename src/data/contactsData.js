790a662edd3426826ffcabd05225cc1149d707ef
sovinbirla <123283605+sovinbirla@users.noreply.github.com>
2025-07-28 16:16:05 -0500

    Updates

--- src/data/contactsData.js
+++
@@ -1,6 +0,0 @@
-export const contactsData = {
-    email: 'sovin.birla123@gmail.com',
-    phone: '+1 4022610764',
-    address: 'Chicago, Illinois',
-    sheetAPI: 'https://sheetdb.io/api/v1/s0iu7eykkv8tw'
-}
